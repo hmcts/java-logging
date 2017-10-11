b787457a508b529abbf0c9b39874da3b85876e5e
Donatas Martinkus <donatasmart@gmail.com>
2017-10-11 11:36:21 +0100

    Unifying coverage report

---
+++ java-logging-dropwizard/src/test/java/uk/gov/hmcts/reform/logging/dropwizard/bundles/LogbackConfigurationBundleTest.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.logging.dropwizard.bundles;
+
+import org.junit.Test;
+
+public class LogbackConfigurationBundleTest {
+
+    @Test
+    public void testRun() throws Exception {
+        //
+    }
+}
