b5c5cecbe882968373df9d4ac830f742be071696
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-10-19 08:56:09 +0100

    Improve coverage for OutboundRequestLoggingInterceptor

--- java-logging-httpcomponents/src/test/java/uk/gov/hmcts/reform/logging/httpcomponents/OutboundRequestLoggingInterceptorTest.java
+++ java-logging-httpcomponents/src/test/java/uk/gov/hmcts/reform/logging/httpcomponents/OutboundRequestLoggingInterceptorTest.java
@@ -7,6 +7,7 @@ import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicStatusLine;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.assertj.core.data.Index;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +19,7 @@ import java.util.Map;
 import static ch.qos.logback.classic.Level.INFO;
 import static net.logstash.logback.marker.Markers.appendEntries;
 import static org.apache.http.protocol.HttpCoreContext.HTTP_TARGET_HOST;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.slf4j.Logger.ROOT_LOGGER_NAME;
 
 public class OutboundRequestLoggingInterceptorTest {
@@ -54,5 +56,22 @@ public class OutboundRequestLoggingInterceptorTest {
         fields.put("responseTime", 20L);
         fields.put("responseCode", 200);
         testAppender.assertEvent(1, INFO, "Outbound request finish", appendEntries(fields));
+    }
+
+    @Test
+    public void allowEmptyConstructorToBuildDefaultClock() throws Exception {
+        testAppender.clearEvents();
+
+        HttpContext context = new BasicHttpContext();
+        context.setAttribute(HTTP_TARGET_HOST, "http://www.google.com");
+
+        OutboundRequestLoggingInterceptor interceptor = new OutboundRequestLoggingInterceptor();
+
+        interceptor.process(new BasicHttpRequest("GET", "/something"), context);
+        interceptor.process(new BasicHttpResponse(new BasicStatusLine(ANY_PROTOCOL, 200, "any")), context);
+
+        assertThat(testAppender.getEvents()).extracting("message")
+            .contains("Outbound request start", Index.atIndex(0))
+            .contains("Outbound request finish", Index.atIndex(1));
     }
 }
