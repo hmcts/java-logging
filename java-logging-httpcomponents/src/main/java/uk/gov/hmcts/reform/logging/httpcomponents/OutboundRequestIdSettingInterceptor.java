aa6ac28ed33f15f25774748cbcfb6b2aec10cda8
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2017-12-15 11:26:08 +0000

    ROC-1105: Set request id on outbound request (#31)

--- java-logging-httpcomponents/src/main/java/uk/gov/hmcts/reform/logging/httpcomponents/OutboundRequestIdSettingInterceptor.java
+++ java-logging-httpcomponents/src/main/java/uk/gov/hmcts/reform/logging/httpcomponents/OutboundRequestIdSettingInterceptor.java
@@ -5,12 +5,27 @@ import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.protocol.HttpContext;
 import uk.gov.hmcts.reform.logging.HttpHeaders;
 import uk.gov.hmcts.reform.logging.MdcFields;
+import uk.gov.hmcts.reform.logging.tracing.RequestIdGenerator;
+
+import java.util.function.Supplier;
 
 public class OutboundRequestIdSettingInterceptor implements HttpRequestInterceptor {
 
+    private Supplier<String> nextRequestId;
+
+    public OutboundRequestIdSettingInterceptor() {
+        this(RequestIdGenerator::next);
+    }
+
+    public OutboundRequestIdSettingInterceptor(Supplier<String> nextRequestId) {
+        this.nextRequestId = nextRequestId;
+    }
+
     @Override
     public void process(HttpRequest request, HttpContext context) {
+        request.setHeader(HttpHeaders.REQUEST_ID, nextRequestId.get());
         request.setHeader(HttpHeaders.ROOT_REQUEST_ID, MdcFields.getRootRequestId());
         request.setHeader(HttpHeaders.ORIGIN_REQUEST_ID, MdcFields.getRequestId());
     }
+
 }
