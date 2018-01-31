0ef357e80448d7d255ef8b752ca966461f33b07e
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-01-31 15:52:48 +0000

    Remove developer mode as default functionality

--- java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/AbstractAppInsights.java
+++ java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/AbstractAppInsights.java
@@ -1,22 +1,24 @@
 package uk.gov.hmcts.reform.logging.appinsights;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.applicationinsights.TelemetryConfiguration;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Application Insights class to send custom telemetry to AppInsights.
+ *
+ * <code>// NO PMD</code> is for pmd rule to skip the check of any abstract methods being present in abstract class.
+ */
 public abstract class AbstractAppInsights { // NOPMD
 
     protected final TelemetryClient telemetry;
 
-    protected AbstractAppInsights(TelemetryClient telemetry, boolean devMode) {
+    protected AbstractAppInsights(TelemetryClient telemetry) {
         requireNonNull(
             telemetry.getContext().getInstrumentationKey(),
             "Missing APPLICATION_INSIGHTS_IKEY environment variable"
         );
 
         this.telemetry = telemetry;
-
-        TelemetryConfiguration.getActive().getChannel().setDeveloperMode(devMode);
     }
 }
--- java-logging-appinsights/src/test/java/uk/gov/hmcts/reform/logging/appinsights/SpringBootTestApplication.java
+++ java-logging-appinsights/src/test/java/uk/gov/hmcts/reform/logging/appinsights/SpringBootTestApplication.java
@@ -1,33 +1,24 @@
 package uk.gov.hmcts.reform.logging.appinsights;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor")
 public class SpringBootTestApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTestApplication.class, args);
     }
 
-    @Configuration
-    public class AppInsightsConfiguration {
-
-        @Bean
-        public TelemetryClient getTelemetryClient() {
-            return new TelemetryClient();
-        }
-    }
-
     @Component
+    @ConditionalOnProperty(name = "app-insights.telemetry-component", havingValue = "true", matchIfMissing = true)
     public static class AppInsightsImp extends AbstractAppInsights {
-        public AppInsightsImp(TelemetryClient telemetry, @Value("${app-insights.dev-mode}") boolean devMode) {
-            super(telemetry, devMode);
+        public AppInsightsImp(TelemetryClient telemetry) {
+            super(telemetry);
         }
     }
 }
