3715eab91c0351b5783ec839ca98b906e7ba3074
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-19 09:42:56 +0000

    Follow same pattern in project for utility classes

--- src/main/java/uk/gov/hmcts/reform/logging/exception/ErrorCode.java
+++ src/main/java/uk/gov/hmcts/reform/logging/exception/ErrorCode.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.logging.exception;
 
-@SuppressWarnings({"squid:S1118", "HideUtilityClassConstructor"})
 public final class ErrorCode {
 
     public static final String UNKNOWN = "UNKNOWN";
+
+    private ErrorCode() {
+    }
 }
