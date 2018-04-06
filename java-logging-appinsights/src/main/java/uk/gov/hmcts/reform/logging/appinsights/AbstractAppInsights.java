3d30e1ea5f5e19ad355a72e3ba74a964f3a82135
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-04-06 15:35:27 +0100

    Introduce AppInsight as an aspect for dependency tracking

--- java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/AbstractAppInsights.java
+++ java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/AbstractAppInsights.java
@@ -1,7 +1,15 @@
 package uk.gov.hmcts.reform.logging.appinsights;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.Duration;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
+import java.time.Instant;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -9,6 +17,7 @@ import static java.util.Objects.requireNonNull;
  *
  * <code>// NO PMD</code> is for pmd rule to skip the check of any abstract methods being present in abstract class.
  */
+@Aspect
 public abstract class AbstractAppInsights { // NOPMD
 
     protected final TelemetryClient telemetry;
@@ -21,5 +30,38 @@ public abstract class AbstractAppInsights { // NOPMD
         telemetry.getContext().getComponent().setVersion(getClass().getPackage().getImplementationVersion());
 
         this.telemetry = telemetry;
+    }
+
+    @Pointcut("@annotation(dependency)")
+    public void dependencyPointCut(Dependency dependency) {
+        // point cut definition
+    }
+
+    @Around("dependencyPointCut(dependency)")
+    public Object trackDependency(ProceedingJoinPoint joinPoint, Dependency dependency) throws Throwable {
+        Instant start = Instant.now();
+
+        try {
+            Object proceed = joinPoint.proceed();
+
+            telemetry.trackDependency(
+                dependency.name(),
+                dependency.command(),
+                new Duration(MILLIS.between(start, Instant.now())),
+                true
+            );
+
+            return proceed;
+        } catch (Throwable exception) {
+            telemetry.trackDependency(
+                dependency.name(),
+                dependency.command(),
+                new Duration(MILLIS.between(start, Instant.now())),
+                false
+            );
+            telemetry.trackException((Exception) exception);
+
+            throw exception;
+        }
     }
 }
