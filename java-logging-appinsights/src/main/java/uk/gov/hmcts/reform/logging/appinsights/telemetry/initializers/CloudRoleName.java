eae63cd79fb9e3b1f0a8f16f18aed5a60b832489
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-04-09 12:05:52 +0100

    Fix a typo

--- java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/telemetry/initializers/CloudRoleName.java
+++ java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/telemetry/initializers/CloudRoleName.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * This is a hack we need until Microsoft delivers proper support for Spring applications. Telemetry initializers
- * defined in ApplicationInsights.xml are not instrumented by spring, we need to expose the application name via
+ * defined in ApplicationInsights.xml are not instrumented by Spring, we need to expose the application name via
  * a static method in order to access it.
  */
 @Component
