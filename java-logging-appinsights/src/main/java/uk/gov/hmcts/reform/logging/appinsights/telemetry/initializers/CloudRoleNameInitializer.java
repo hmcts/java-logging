ddf72ffe8694fd309e40aa8d6e0521e4464a2909
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-04-09 12:04:07 +0100

    Use correct logging

--- java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/telemetry/initializers/CloudRoleNameInitializer.java
+++ java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/telemetry/initializers/CloudRoleNameInitializer.java
@@ -3,15 +3,18 @@ package uk.gov.hmcts.reform.logging.appinsights.telemetry.initializers;
 import com.microsoft.applicationinsights.extensibility.context.ContextTagKeys;
 import com.microsoft.applicationinsights.telemetry.Telemetry;
 import com.microsoft.applicationinsights.web.extensibility.initializers.WebTelemetryInitializerBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CloudRoleNameInitializer extends WebTelemetryInitializerBase {
+    private static final Logger logger = LoggerFactory.getLogger(CloudRoleNameInitializer.class);
+
     @Override
     protected void onInitializeTelemetry(Telemetry telemetry) {
         if (CloudRoleName.get() != null) {
-            telemetry.getContext().getTags().put(ContextTagKeys.getKeys().getDeviceRoleName(),
-                CloudRoleName.get());
+            telemetry.getContext().getTags().put(ContextTagKeys.getKeys().getDeviceRoleName(), CloudRoleName.get());
         } else {
-            System.out.println("It's null");
+            logger.warn("Cloud role name not provided");
         }
     }
 }
