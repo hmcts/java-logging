8153218fa7e24e8a9890de433a30d52ad3655f23
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2018-04-15 15:13:17 +0200

    Update to Spring Boot 2 (#63)

--- java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/AppInsightsRequestComponentConfiguration.java
+++ java-logging-appinsights/src/main/java/uk/gov/hmcts/reform/logging/appinsights/AppInsightsRequestComponentConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
