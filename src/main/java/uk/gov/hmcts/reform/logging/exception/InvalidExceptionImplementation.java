ba78e1e793b86a23bd0d3374c2eed6dd374424c5
Kevlar <kevin.j.cross@gmail.com>
2018-12-18 08:59:27 +0000

    simplified the documentation.

--- src/main/java/uk/gov/hmcts/reform/logging/exception/InvalidExceptionImplementation.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.logging.exception;
-//Will be removed in future enhancement revision
-@Deprecated
-public class InvalidExceptionImplementation extends AbstractLoggingException {
-
-    public InvalidExceptionImplementation(String message, Throwable cause) {
-        super(AlertLevel.P1, "0", message, cause);
-    }
-}
