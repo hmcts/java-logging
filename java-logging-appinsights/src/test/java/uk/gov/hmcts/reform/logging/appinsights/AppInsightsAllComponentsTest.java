0db9c6cdd96acc8ddee4e92b5a92f25e77cd6ca9
donatas M <1420191+doncem@users.noreply.github.com>
2019-02-23 18:52:38 +0000

    Fix dependencies (#109)
    * Update spring dependency management plugin
    
    * Update owasp plugin
    
    * Align spring boot versions + bump
    
    * Update checkstyle plugin
    
    * Update PMD plugin
    
    * Update jacoco plugin
    
    * Suppress shadowed dependency by/from appinsights

--- java-logging-appinsights/src/test/java/uk/gov/hmcts/reform/logging/appinsights/AppInsightsAllComponentsTest.java
+++ java-logging-appinsights/src/test/java/uk/gov/hmcts/reform/logging/appinsights/AppInsightsAllComponentsTest.java
@@ -1,9 +1,6 @@
 package uk.gov.hmcts.reform.logging.appinsights;
 
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,22 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {
+    "azure.applicationinsights.instrumentationkey=some-key"
+})
 public class AppInsightsAllComponentsTest {
-
-    @ClassRule
-    public static EnvironmentVariables variables = new EnvironmentVariables();
 
     @Autowired
     private AbstractAppInsights insights;
 
     @Autowired
     private ApplicationContext context;
-
-    @BeforeClass
-    public static void setUp() {
-        variables.set("AZURE_APPLICATIONINSIGHTS_INSTRUMENTATIONKEY", "some-key");
-    }
 
     @Test
     public void contextLoads() {
